91a0a495dc58dd66731d6afa6ae0c0ab0783a035
Brian Wangora <brianwangora@gmail.com>
2022-07-27 22:52:47 +0300

    more navbar edits made

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,9 +5,9 @@ export default function Navbar(){
     return(
         <nav className="navbar bg-grey">
             <div className="container-fluid">
-                <Link className="nav-link" to="/">HOME || </Link>
-                <Link className="nav-link" to="/contact">CONTACT || </Link>
-                <Link className="nav-link" to="/inventory">INVENTORY || </Link>
+                <Link className="nav-link" to="/">| HOME | </Link>
+                <Link className="nav-link" to="/contact">| CONTACT | </Link>
+                <Link className="nav-link" to="/inventory">| INVENTORY | </Link>
             </div>
         </nav>
     )
