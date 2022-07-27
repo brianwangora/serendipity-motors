eda56987b2d09579269e3348cb1922fe98d04b53
Brian Wangora <brianwangora@gmail.com>
2022-07-27 15:11:05 +0300

    description of project added

---
+++ DESCRIPTION.md
@@ -0,0 +1,17 @@
+# SERENDIPITY MOTORS
+
+## API
+The API used is: ""
+
+This is a single web application for a car dealership called Serendipity Motors.
+it has 3 client-side routes;
+
+## 1. Home
+Here the user is presented with information about the dealership and can toggle dark mode or light mode depending on preference.
+
+## 2. Contact
+Here the user finds contact information belonging to the dealership.
+
+## 3. Inventory
+Here the user finds the dealership's inventory which is obtained using the fetch method to obtain data from the API.
+by clicking on the "DETAILS" button, the user is directed to a page that contains all the information about the car that has been clicked.
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,17 +5,9 @@ export default function Navbar(){
     return(
         <nav className="navbar bg-light">
             <div className="container-fluid">
-                {/* <ul className="navbar-nav">
-                    <li className="nav-item"> */}
-                        <Link className="nav-link" to="/">HOME || </Link>
-                    {/* </li> */}
-                    {/* <li className="nav-item"> */}
-                        <Link className="nav-link" to="/contact">CONTACT || </Link>
-                    {/* </li> */}
-                    {/* <li className="nav-item"> */}
-                        <Link className="nav-link" to="/inventory">INVENTORY || </Link>
-                    {/* </li> */}
-                {/* </ul> */}
+                <Link className="nav-link" to="/">HOME || </Link>
+                <Link className="nav-link" to="/contact">CONTACT || </Link>
+                <Link className="nav-link" to="/inventory">INVENTORY || </Link>
             </div>
         </nav>
     )
